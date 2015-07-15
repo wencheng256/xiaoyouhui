1ab30fa66237754b8155ccd767b9db7e72049746
Wencheng <wencheng256@gmail.com>
2015-07-16 00:06:44 +0800

    进行一系列的配置和修改

--- xiaoyouhui/src/com/wencheng/utils/Mysql.java
+++ xiaoyouhui/src/com/wencheng/utils/Mysql.java
@@ -1,10 +1,15 @@
 package com.wencheng.utils;
 
-import org.apache.log4j.Logger;
-
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
+
+import org.apache.log4j.Logger;
 
 public class Mysql {
 	/**
@@ -13,6 +18,17 @@ public class Mysql {
 	private static final Logger logger = Logger.getLogger(Mysql.class);
 	
 	private static Mysql mysql;
+	private Connection conn;
+	private Statement stat;
+	
+	static{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		}
+	}
 	
 	private Mysql()
 	{
@@ -25,8 +41,28 @@ public class Mysql {
 			logger.warn("Mysql() - 找不到数据库配置文件", e); //$NON-NLS-1$
 		}
 		
+		String url = prop.getProperty("url");
+		String username = prop.getProperty("username");
+		String password = prop.getProperty("password");
+		
+		try {
+			conn = DriverManager.getConnection(url, username, password);
+			stat = conn.createStatement();
+		} catch (SQLException e) {
+			// TODO 自动生成的 catch 块
+
+			logger.warn("Mysql() - 数据库连接失败", e); //$NON-NLS-1$
+		}
+		
+		
+		
+		
 	}
 	
+	/**
+	 * 获取单例的Mysql对象
+	 * @return
+	 */
 	public static Mysql newInstance()
 	{
 		if(mysql == null)
@@ -35,5 +71,45 @@ public class Mysql {
 		}
 		return mysql;
 	}
+	
+	/**
+	 * 执行查询语句
+	 * @param sql
+	 * @return 查询结果
+	 */
+	public ResultSet query(String sql)
+	{
+		try {
+			return stat.executeQuery(sql);
+		} catch (SQLException e) {
+			// TODO 自动生成的 catch 块
 
+			logger.warn("query(String) - 数据查询出错", e); //$NON-NLS-1$
+
+			return null;
+		}
+	}
+	
+	/**
+	 * 执行插入等返回布尔值的语句
+	 * @param sql
+	 * @return	boolean
+	 */
+	public boolean execute(String sql)
+	{
+		return mysql.execute(sql);
+	}
+	
+	
+	/**
+	 * 执行更新语句
+	 * @param sql
+	 * @return
+	 */
+	public int executeupdate(String sql)
+	{
+		return mysql.executeupdate(sql);
+	}
+	
+	
 }
